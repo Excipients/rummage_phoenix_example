80c741bf8602bd4aab67cc39c483ed126ccfbaae
Adi Iyengar <aditya7iyengar@gmail.com>
2018-09-07 09:49:09 -0400

    Remove comments

--- example/config/config.exs
+++ example/config/config.exs
@@ -1,15 +1,8 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# General application configuration
 config :example,
   ecto_repos: [Example.Repo]
 
-# Configures the endpoint
 config :example, ExampleWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "do+7+hQgVaFTCYXJPcw42rH0VtMm2f6HVkYoEDFEQpjdWqncpQU8jakrIsTf17GZ",
@@ -17,11 +10,8 @@ config :example, ExampleWeb.Endpoint,
   pubsub: [name: Example.PubSub,
            adapter: Phoenix.PubSub.PG2]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
--- example/config/dev.exs
+++ example/config/dev.exs
@@ -21,15 +21,13 @@ config :example, ExampleWeb.Endpoint,
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
-# Set a higher stacktrace during development. Avoid configuring such
-# in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
 config :example, Example.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("POSTGRES_USER"),
   password: System.get_env("POSTGRES_PASSWORD"),
   database: "example_dev",
   hostname: "localhost",
+  timeout: 300_000,
   pool_size: 10
