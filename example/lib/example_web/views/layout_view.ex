8eb04433729d6fce827efd45c6248730c4fcd27c
Adi Iyengar <aditya7iyengar@gmail.com>
2018-09-27 12:34:09 -0400

    Add UI to interact with rummage examples

--- example/lib/example_web/views/layout_view.ex
+++ example/lib/example_web/views/layout_view.ex
@@ -1,3 +1,20 @@
 defmodule ExampleWeb.LayoutView do
   use ExampleWeb, :view
+
+  def navbar_link(conn, href: href, title: title) do
+    class = get_class(conn.path_info, href)
+    raw(
+    """
+    <li class="tab"><a class="#{class}" href="#{href}">#{title}</a></li>
+    """
+    )
+  end
+
+  defp get_class(["categories_bootstrap" | _tail], "/categories_bootstrap") do
+    "active"
+  end
+
+  defp get_class([], "/"), do: "active"
+
+  defp get_class(_path_info, _href), do: ""
 end
